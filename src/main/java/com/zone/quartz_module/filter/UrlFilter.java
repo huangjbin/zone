0a9e73a90d30600acf5d12500a6267d70b7577ae
huangjunbin <159357xx>
2019-03-27 18:00:34 +0800

    mm

--- src/main/java/com/zone/quartz_module/filter/UrlFilter.java
+++ src/main/java/com/zone/quartz_module/filter/UrlFilter.java
@@ -25,7 +25,7 @@ import java.util.Map;
 public class UrlFilter implements Filter {
 
     @Autowired
-    HttpAPIService httpAPIService;//n1 n2  n3   n4
+    HttpAPIService httpAPIService;//n1 n2  n3   n4  555
 
     @Autowired
     ObjectMapper objectMapper;
